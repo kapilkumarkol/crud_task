fdd8637c4b880b92b01042b4be7e5b08f00eae7f
kapilkumarkol <99422222+kapilkumarkol@users.noreply.github.com>
2024-10-01 21:04:13 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('task_list');
+    return view('welcome');
 });
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'login']);
